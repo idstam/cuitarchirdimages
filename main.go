fd571410d732d012fba233330b8b8e7a55b51a0e
Johan Idstam <johan@idstam.com>
2019-10-03 13:25:15 +0200

    Add left handed chords

--- main.go
+++ main.go
@@ -36,18 +36,71 @@ func main() {
 		panic(err)
 	}
 	for _, chord := range chords {
-		dc := getEmptyDiagram()
+		dc := getEmptyDiagramRH()
 		if !strings.Contains(chord, ":") {
 			fmt.Println("Invalid chord " + chord)
 			return
 		}
 		chordName := strings.Split(chord, ":")[0]
-		drawChord(dc, chord, chordName)
-		dc.SavePNG("chordImages/" + chordName + ".png")
+		drawChordRH(dc, strings.Split(chord, ":")[1], chordName)
+		dc.SavePNG("chordImages/" + chordName + "_RH.png")
+	}
+
+	chords, err = readLines("chords.txt")
+	if err != nil {
+		panic(err)
+	}
+	for _, chord := range chords {
+		dc := getEmptyDiagramLH()
+		if !strings.Contains(chord, ":") {
+			fmt.Println("Invalid chord " + chord)
+			return
+		}
+		chordName := strings.Split(chord, ":")[0]
+		drawChordLH(dc, strings.Split(chord, ":")[1], chordName)
+		dc.SavePNG("chordImages/" + chordName + "_LH.png")
+	}
+
+}
+
+func drawChordLH(dc *gg.Context, chord, chordName string) {
+	fingers := strings.Split(chord, ",")
+	minFret := 99
+	maxFret := 5
+
+	for stringNumber, finger := range fingers {
+		if strings.Contains("xo", finger) {
+			continue
+		}
+		if stringNumber < minFret {
+			minFret = stringNumber
+		}
+		if stringNumber > maxFret {
+			maxFret = stringNumber
+		}
+	}
+	if maxFret == 5 {
+		minFret = 1
+	}
+	fretHeight := drawFrets(dc, minFret, maxFret)
+	drawChordName(dc, chordName)
+	for stringNumber, finger := range fingers {
+		switch finger {
+		case "o":
+			drawOpenString(dc, float64(5-stringNumber))
+		case "x":
+			drawMutedString(dc, float64(5-stringNumber))
+
+		default:
+			fretNumber, err := strconv.Atoi(finger)
+			if err == nil {
+				drawPressedString(dc, float64(5-stringNumber), fretHeight, fretNumber)
+			}
+		}
 	}
 }
 
-func drawChord(dc *gg.Context, chord, chordName string) {
+func drawChordRH(dc *gg.Context, chord, chordName string) {
 	fingers := strings.Split(chord, ",")
 	minFret := 99
 	maxFret := 5
@@ -98,8 +151,8 @@ func drawChordName(dc *gg.Context, chordName string) {
 	w, _ := dc.MeasureString(chordName)
 	midFret := (float64(width) - (firstString * 2)) / 2
 	midLabel := w / 2
-	x := midFret - midLabel + firstString
-
+	x := midFret - midLabel
+	//if err := dc.LoadFontFace("fonts/dealerplate_california/dealerplate california.ttf", 40); err != nil {
 	if err := dc.LoadFontFace("fonts/assurant_standard/Assurant-Standard.ttf", 40); err != nil {
 		panic(err)
 	}
@@ -131,7 +184,7 @@ func drawMutedString(dc *gg.Context, stringNumber float64) {
 	dc.Stroke()
 }
 
-func getEmptyDiagram() *gg.Context {
+func getEmptyDiagramRH() *gg.Context {
 
 	dc := gg.NewContext(width, height)
 	//Background
@@ -155,6 +208,34 @@ func getEmptyDiagram() *gg.Context {
 		stringX = stringX + stringSpacing
 		dc.DrawRectangle(stringX, nutY, 2, maxY)
 		dc.Fill()
+	}
+
+	return dc
+}
+func getEmptyDiagramLH() *gg.Context {
+
+	dc := gg.NewContext(width, height)
+	//Background
+	dc.SetRGB(1, 1, 1)
+	dc.DrawRectangle(0, 0, maxX+1, maxY)
+	dc.Fill()
+
+	//Nut
+	dc.SetRGB(redByte, 0, 0)
+	dc.DrawRectangle(firstString+1, nutY, (stringSpacing * 5), 4)
+	dc.Stroke()
+
+	//Strings
+
+	stringX := firstString
+
+	dc.DrawRectangle(stringX+(stringSpacing*5)-3, nutY, 3, maxY)
+	dc.Fill()
+
+	for i := 0; i < 6; i++ {
+		dc.DrawRectangle(stringX, nutY, 2, maxY)
+		dc.Fill()
+		stringX = stringX + stringSpacing
 	}
 
 	return dc
