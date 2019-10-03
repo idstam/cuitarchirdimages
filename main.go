4deda2840f0b3b175a6aa69a7804c38cce6e3432
Johan Idstam <johan@idstam.com>
2019-10-03 15:45:27 +0200

    Write string names

--- main.go
+++ main.go
@@ -18,16 +18,20 @@ var width int
 var height int
 var maxX float64
 var maxY float64
+var fontPath string
 
 func init() {
 	width = 200
 	height = 400
 	maxX = 199.
-	maxY = 399.
+	maxY = 375.
 	firstString = 15.
 	stringSpacing = 34.
 	redByte = 0.
-	nutY = 75.
+	nutY = 65.
+	//fontPath = "fonts/assurant_standard/Assurant-Standard.ttf"
+	//fontPath = "fonts/dealerplate_california/dealerplate california.ttf"
+	fontPath = "fonts/techna_sans/TechnaSans-Regular.ttf"
 }
 func main() {
 
@@ -36,38 +40,29 @@ func main() {
 		panic(err)
 	}
 	for _, chord := range chords {
-		dc := getEmptyDiagramRH()
 		if !strings.Contains(chord, ":") {
 			fmt.Println("Invalid chord " + chord)
 			return
 		}
 		chordName := strings.Split(chord, ":")[0]
-		drawChordRH(dc, strings.Split(chord, ":")[1], chordName)
+
+		//Lefty
+		dc := getEmptyDiagram(true)
+		drawChord(dc, strings.Split(chord, ":")[1], chordName, true)
+		dc.SavePNG("chordImages/" + chordName + "_LH.png")
+
+		//Righty
+		dc = getEmptyDiagram(false)
+		drawChord(dc, strings.Split(chord, ":")[1], chordName, false)
 		dc.SavePNG("chordImages/" + chordName + "_RH.png")
 	}
 
-	chords, err = readLines("chords.txt")
-	if err != nil {
-		panic(err)
-	}
-	for _, chord := range chords {
-		dc := getEmptyDiagramLH()
-		if !strings.Contains(chord, ":") {
-			fmt.Println("Invalid chord " + chord)
-			return
-		}
-		chordName := strings.Split(chord, ":")[0]
-		drawChordLH(dc, strings.Split(chord, ":")[1], chordName)
-		dc.SavePNG("chordImages/" + chordName + "_LH.png")
-	}
-
 }
 
-func drawChordLH(dc *gg.Context, chord, chordName string) {
+func drawChord(dc *gg.Context, chord, chordName string, isLeft bool) {
 	fingers := strings.Split(chord, ",")
 	minFret := 99
 	maxFret := 5
-
 	for stringNumber, finger := range fingers {
 		if strings.Contains("xo", finger) {
 			continue
@@ -85,77 +80,58 @@ func drawChordLH(dc *gg.Context, chord, chordName string) {
 	fretHeight := drawFrets(dc, minFret, maxFret)
 	drawChordName(dc, chordName)
 	for stringNumber, finger := range fingers {
+		stringToUse := stringNumber
+		if isLeft {
+			stringToUse = 5 - stringNumber
+		}
 		switch finger {
 		case "o":
-			drawOpenString(dc, float64(5-stringNumber))
+			drawOpenString(dc, float64(stringToUse))
 		case "x":
-			drawMutedString(dc, float64(5-stringNumber))
+			drawMutedString(dc, float64(stringToUse))
 
 		default:
 			fretNumber, err := strconv.Atoi(finger)
 			if err == nil {
-				drawPressedString(dc, float64(5-stringNumber), fretHeight, fretNumber)
+				drawPressedString(dc, float64(stringToUse), fretHeight, fretNumber)
 			}
 		}
 	}
 }
 
-func drawChordRH(dc *gg.Context, chord, chordName string) {
-	fingers := strings.Split(chord, ",")
-	minFret := 99
-	maxFret := 5
-
-	for stringNumber, finger := range fingers {
-		if strings.Contains("xo", finger) {
-			continue
-		}
-		if stringNumber < minFret {
-			minFret = stringNumber
-		}
-		if stringNumber > maxFret {
-			maxFret = stringNumber
-		}
-	}
-	if maxFret == 5 {
-		minFret = 1
-	}
-	fretHeight := drawFrets(dc, minFret, maxFret)
-	drawChordName(dc, chordName)
-	for stringNumber, finger := range fingers {
-		switch finger {
-		case "o":
-			drawOpenString(dc, float64(stringNumber))
-		case "x":
-			drawMutedString(dc, float64(stringNumber))
-
-		default:
-			fretNumber, err := strconv.Atoi(finger)
-			if err == nil {
-				drawPressedString(dc, float64(stringNumber), fretHeight, fretNumber)
-			}
-		}
-	}
-}
 func drawFrets(dc *gg.Context, minFret, maxFret int) float64 {
 	fretCount := maxFret - minFret + 1
-	fretHeight := (float64(height) - nutY - 2) / float64(fretCount)
-	for i := 1; i <= fretCount; i++ {
+	fretHeight := (maxY - nutY - 2) / float64(fretCount)
+	for i := 0; i <= fretCount; i++ {
 		dc.SetRGB(redByte, 0, 0)
-		dc.DrawRectangle(firstString, nutY+(fretHeight*float64(i)), (stringSpacing * 5), 3)
+		dc.DrawRectangle(firstString, nutY+(fretHeight*float64(i)), (stringSpacing*5)+2, 3)
 		dc.Fill()
+		if i < fretCount {
+			drawFretNumber(dc, i, fretHeight)
+		}
 	}
+
 	return fretHeight
 }
 
+func drawFretNumber(dc *gg.Context, fretNumber int, fretHeight float64) {
+	if err := dc.LoadFontFace(fontPath, 15); err != nil {
+		panic(err)
+	}
+	fretNumStr := strconv.Itoa(fretNumber + 1)
+	x := firstString - 10
+
+	dc.DrawString(fretNumStr, x, nutY+(fretHeight*float64(fretNumber))+20)
+	dc.Fill()
+}
 func drawChordName(dc *gg.Context, chordName string) {
+	if err := dc.LoadFontFace(fontPath, 40); err != nil {
+		panic(err)
+	}
 	w, _ := dc.MeasureString(chordName)
 	midFret := (float64(width) - (firstString * 2)) / 2
 	midLabel := w / 2
 	x := midFret - midLabel
-	//if err := dc.LoadFontFace("fonts/dealerplate_california/dealerplate california.ttf", 40); err != nil {
-	if err := dc.LoadFontFace("fonts/assurant_standard/Assurant-Standard.ttf", 40); err != nil {
-		panic(err)
-	}
 
 	dc.DrawString(chordName, x, (nutY / 2))
 	dc.Fill()
@@ -184,58 +160,44 @@ func drawMutedString(dc *gg.Context, stringNumber float64) {
 	dc.Stroke()
 }
 
-func getEmptyDiagramRH() *gg.Context {
+func drawStringName(dc *gg.Context, stringNumber int, isLeft bool) {
 
-	dc := gg.NewContext(width, height)
-	//Background
-	dc.SetRGB(1, 1, 1)
-	dc.DrawRectangle(0, 0, maxX+1, maxY)
-	dc.Fill()
-
-	//Nut
-	dc.SetRGB(redByte, 0, 0)
-	dc.DrawRectangle(firstString+1, nutY, (stringSpacing * 5), 4)
-	dc.Stroke()
-
-	//Strings
-
-	stringX := firstString
-
-	dc.DrawRectangle(stringX, nutY, 3, maxY)
-	dc.Fill()
-
-	for i := 1; i < 6; i++ {
-		stringX = stringX + stringSpacing
-		dc.DrawRectangle(stringX, nutY, 2, maxY)
-		dc.Fill()
+	if err := dc.LoadFontFace(fontPath, 15); err != nil {
+		panic(err)
 	}
-
-	return dc
+	x := firstString + (float64(stringNumber) * stringSpacing) - 4
+	stringNames := []string{"E", "A", "D", "G", "B", "e"}
+	if isLeft {
+		stringNames = []string{"e", "B", "G", "D", "A", "E"}
+	}
+	dc.DrawString(stringNames[stringNumber], x, maxY+15)
+	dc.Fill()
 }
-func getEmptyDiagramLH() *gg.Context {
+
+func getEmptyDiagram(isLeft bool) *gg.Context {
 
 	dc := gg.NewContext(width, height)
 	//Background
 	dc.SetRGB(1, 1, 1)
-	dc.DrawRectangle(0, 0, maxX+1, maxY)
+	dc.DrawRectangle(0, 0, float64(width), float64(height))
 	dc.Fill()
 
-	//Nut
 	dc.SetRGB(redByte, 0, 0)
-	dc.DrawRectangle(firstString+1, nutY, (stringSpacing * 5), 4)
-	dc.Stroke()
-
-	//Strings
 
 	stringX := firstString
-
-	dc.DrawRectangle(stringX+(stringSpacing*5)-3, nutY, 3, maxY)
+	thickE := 4.
+	if isLeft {
+		dc.DrawRectangle(stringX+(stringSpacing*5)-thickE, nutY, thickE, maxY-nutY)
+	} else {
+		dc.DrawRectangle(stringX, nutY, thickE, maxY-nutY)
+	}
 	dc.Fill()
 
 	for i := 0; i < 6; i++ {
-		dc.DrawRectangle(stringX, nutY, 2, maxY)
+		dc.DrawRectangle(stringX, nutY, 2, maxY-nutY)
 		dc.Fill()
 		stringX = stringX + stringSpacing
+		drawStringName(dc, i, isLeft)
 	}
 
 	return dc
